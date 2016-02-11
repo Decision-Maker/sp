56cba6639f1da4c554133c25d4ac4911f147c0a8
Devon Call <calld2@quest-OptiPlex-9020>
2016-02-10 19:11:57 -0800

    changed options from a list of objects to a list of strings and fixed ng-repeat so that it doesn't ignore duplicates, also implemented a filter on the options so that it finds and removes duplicates. fixed html so that options of a room display under the room name as an unordered list

--- app.js
+++ app.js
@@ -8,41 +8,66 @@ app.factory('room', [function(){
     rooms: []
   };
   return o;
-}])
+}]);
 
 app.controller('MainCtrl', [
 '$scope',
 'room',
 function($scope, room){
   $scope.rooms = room.rooms;
-  $scope.options = []
+  $scope.options = [];
 
   $scope.addRoom = function(){
     if(!$scope.title || $scope.title === '') { return; }
 
-    var forms = document.getElementsByClassName("option");
-    var length = forms.length;
-    var seen = [];
-    var valid = true;
-    r = []
+    //var forms = document.getElementsByClassName("option");
+    //var length = forms.length;
+    //var seen = [];
+    //var valid = true;
+    var uniqueOptions = $scope.options.filter(function(item, pos, self){
+      for(i = pos + 1; i < self.length; i++){
+        if(self[i] == item){
+          return false;
+        }
+      }
+      return true;
+    });
+
+    /*for(option in $scope.options){
+
+    }*/
+
+    /*r = []
     for (i = 0; i < length; i++){
       r.push(forms[i].value)
-    }
+    }*/
 
-    alert(r)
+    alert(uniqueOptions);
     $scope.rooms.push(
       {
         title: $scope.title,
-        options: r
+        options: uniqueOptions
       }
     );
     $scope.title = '';
+    $scope.options = [];
   };
 
   $scope.addOption = function(event, limit = false){
+    //alert(event.keyCode);
     if(limit || event.keyCode == 13){
-        $scope.options.push({title: ""});
+        $scope.options.push('');
     }
+  };
+
+  $scope.foo = function(){
+    var j = ['','a','b','c'];
+    var k = [];
+    for(i in j){
+      k.push(j[i]);
+    }
+    alert(k);
+
   };
 
 }]);

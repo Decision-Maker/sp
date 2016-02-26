a48c53c51b1802ab5b3e58d95ce62c20efe77baf
adeeperb1ue <jessesliter@gmail.com>
2016-02-26 12:38:09 -0800

    fixed some minor css issues, reenabled drag and drop

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -56,7 +56,7 @@ function($scope, room){
   //Used for drag and drop
   $scope.barConfig = {
       animation: 150,
-      handle: ".handle",
+      //handle: ".handle",
       onSort: function (/** ngSortEvent */evt){
           // @see https://github.com/RubaXa/Sortable/blob/master/ng-sortable.js#L18-L24
       }

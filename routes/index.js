7d088309bb60d48662f11724262d21d53924db69
Paige Ormiston <Paige@wlan061-236.wlan.wwu.edu>
2016-02-26 13:00:36 -0800

    added deleted file

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/*router.get('/vote/:roomID', function(req, res, next) {
+  res.send(req.param.roomID);
+});*/
+
+module.exports = router;

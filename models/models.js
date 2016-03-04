434555bbad8c2d7bd5e517735ad3cac89b80c07c
Devon Call <calld2@quest-OptiPlex-9020>
2016-03-04 15:39:55 -0800

    added back in models.js

---
+++ models/models.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String,
+  id: ObjectId
+});
+
+var participantSchema = new Schema({
+  userId: ObjectId,
+  roomId: ObjectId,
+  optionId: ObjectId
+});
+
+var roomSchema = new Schema({
+  title: String,
+  roomId: ObjectId
+});
+
+var optionSchema = new Schema({
+  optionId: ObjectId,
+  roomId: ObjectId,
+  name: String
+});
+
+mongoose.model('Option', optionSchema);
+mongoose.model('Room', roomSchema);
+var Participant = mongoose.model('Participant', participantSchema);
+mongoose.model('User', userSchema);
+
+optionSchema.virtual('count').get(function(){
+  return Participant.where({'roomId':this.roomId,'optionId':this.optionId} ).count();
+});

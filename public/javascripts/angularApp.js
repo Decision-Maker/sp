dd1e08851d87412ed22dab251db226252590598b
Jesse Sliter <sliterj@sliterj-macbookpro.roam.corp.google.com>
2016-04-27 14:00:46 -0700

    created views and controllers for login and register pages

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -75,7 +75,7 @@ app.factory('auth', ['$http', '$window', function($http,$window){
     };
 
     auth.register = function(user){
-      return $http.post('/register', user).success(function(data){
+      return $http.post('register', user).success(function(data){
         auth.saveToken(data.token);
       });
     };
@@ -108,6 +108,30 @@ function($stateProvider, $urlRouterProvider) {
           return rooms.getAll();
         }]
       }
+    });
+
+    $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
+    })
+
+    $stateProvider
+    .state('register', {
+      url: '/register',
+      templateUrl: '/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
     });
 
     $stateProvider
@@ -190,6 +214,39 @@ function($scope, rooms, room){
     //$scope.body = '';
   };
 
+}]);
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
 }]);
 
 app.controller('MainCtrl', [

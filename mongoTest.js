6f5b5982768a68f410e41b7bcfdb55dae5e22a9a
Devon Call <calld2@quest-OptiPlex-9020>
2016-05-08 16:58:09 -0700

    testing out javascrpit promises in test code

--- mongoTest.js
+++ mongoTest.js
@@ -22,43 +22,50 @@ var polls = [{title: "best ale", options: ["brown","red","blonde","dark","light"
 
 var userObj = [];
 
-
-for(var i = 0; i < users.length; i++){
-    var nu = new db.model.User();
-    nu.name = users[i].name;
-    nu.setPassword(users[i].password);
-    nu.save(function(err){
-        if(err){console.log("error on save");}
-        else{
-            
-console.log("saved " + nu.name);
-        }
-    });
-    userObj.push(nu);
-}
-
 function getIDfromList(name, list){
     var user = list.filter(function(x){return x.name === name})[0];
     return user._id;
 }
 
-db.model.User.findOne({name: "Barikhik"}, function(err, us){
-    if(err){console.log("ERROR");}
-    console.log(us._id.equals(getIDfromList("Barikhik", userObj)));
+var saveUsers = new Promise(function(resolve, reject){
+  var error = false;
+  for(var i = 0; i < users.length; i++){
+      var nu = new db.model.User();
+      nu.name = users[i].name;
+      nu.setPassword(users[i].password);
+      nu.save(function(err){
+          if(err){error == true}
+      });
+      userObj.push(nu);
+  }
+  if(error){
+    reject(new Error("problem on save"));
+  }else{
+    resolve("");
+  }
 });
+
+saveUsers.then(function(res){
+  db.model.User.findOne({name: "Barikhik"}, function(err, us){
+      if(err){console.log("ERROR");}
+      console.log(us._id.equals(getIDfromList("Barikhik", userObj)));
+  });
+  return res;
+}).then(function(res){
+  for(var i = 0; i < users.length; i++){
+      db.model.User.remove({name: users[i].name}, function(err){
+          if(err){console.log("Error on delete");}
+          else{
+              console.log("removed " + users[i].name);
+          }
+      });
+  }
+  return res;
+}, function(err){console.error(err);});
+
 
 
 // for(var i = 0; i < polls.length; i++){
 //     var nu = new db.model.Room();
 //     // nu.title
 // }
-
-for(var i = 0; i < users.length; i++){
-    db.model.User.remove({name: users[i].name}, function(err){
-        if(err){console.log("Error on delete");}
-        else{
-            console.log("removed " + users[i].name);
-        }
-    });
-}
-

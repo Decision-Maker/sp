eda8149bf56627902e5ebc38c1cd1f701bf33136
Devon Call <calld2@quest-OptiPlex-9020>
2016-05-17 13:23:32 -0700

    adding test code

---
+++ server_test/clearData.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var db = require('../models/models');
+
+mongoose.connect('mongodb://admin:123456@ds019268.mlab.com:19268/votingrooms');
+
+var o = {};
+
+o.go = function(){
+	var empty = new Promise(function(resolve, reject){
+		db.model.User.remove({}, function(err){
+			if(err){
+				console.error(err);
+			}
+			db.model.Option.remove({}, function(err){
+				if(err){
+					console.error(err);
+				}
+				db.model.Observe.remove({}, function(err){
+					if(err){
+						console.error(err);
+					}
+					db.model.Room.remove({}, function(err){
+						if(err){
+							console.error(err);
+						}
+						db.model.Vote.remove({}, function(err){
+							if(err){
+								console.error(err);
+							}
+							resolve("");
+						});
+					})
+				});
+			});
+		});
+	});
+	empty.then(function(res){
+		process.exit(0);
+	});
+};
+
+module.exports = o;

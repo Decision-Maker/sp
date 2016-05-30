7edc107396fbf75ad9999d1e50a8b880703be359
huntere6 <huntere6@students.wwu.edu>
2016-05-30 16:24:42 -0700

    results page displays users, shows multiple winning option if there are ties

--- public/javascripts/Controllers/resultController.js
+++ public/javascripts/Controllers/resultController.js
@@ -8,23 +8,62 @@ function($scope, rooms, room, results){
   $scope.room = room;
   $scope.results = results;
 
-  console.log(results);
+  // console.log(results);
 
   $scope.getWinners = function(){
     var topscore = $scope.results.result[0].count;
     var w = []
     for(var i = 0; i < $scope.results.result.length; i++){
       if($scope.results.result[i].count === topscore){
-         w.push($scope.results.result[i]);
+         var thisObj = {};
+         var longCount = $scope.results.result[i].count;
+         var shortenedCount;
+         if(room.voteType == "FPP"){
+            shortenedCount = longCount; // dealing with integers so no change
+         } else {
+            shortenedCount = longCount.toFixed(2); // 2 decimal places max
+         }
+         console.log("shortenedCount", shortenedCount);
+         var resultTitle = $scope.results.result[i].title;
+         thisObj.title = resultTitle;
+         thisObj.count = shortenedCount;
+         w.push(thisObj);
       }
     }
+    console.log("W", w);
     return w;
   }
 
   $scope.winners = $scope.getWinners();
 
-  console.log($scope.winners);
+  $scope.getTie = function(){
+     if($scope.winners.length == 1){
+        return "";
+     } else {
+        return "tie";
+     }
+  }
 
+  $scope.tie = $scope.getTie();
+
+  $scope.pointsVotes = function(){
+     if(room.voteType == "Borda"){
+        if($scope.winners[0].count > 1){
+           return "points";
+        } else {
+           return "point";
+        }
+     }
+     if(room.voteType == "FPP"){
+        if($scope.winners[0].count > 1){
+           return "votes";
+        } else {
+           return "vote";
+        }
+     }
+  }
+
+  $scope.pointsVotesString = $scope.pointsVotes();
 }]);
 
 app.controller('ProfileCtrl', [
